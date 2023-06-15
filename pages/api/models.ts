f35e2ce04e1e8912f15077fad8ed4cccb9050002
CodeCraft3r <72947633+codecraft3r@users.noreply.github.com>
2023-06-14 18:31:28 -0700

    "i'm a teapot"

--- pages/api/models.ts
+++ pages/api/models.ts
@@ -1,28 +1,42 @@
 import { OPENAI_API_HOST, OPENAI_API_TYPE, OPENAI_API_VERSION, OPENAI_ORGANIZATION } from '@/utils/app/const';
 
+import { getDataSource, getUser } from '@/utils/server/rdbms';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
+
 import { OpenAIModel, OpenAIModelID, OpenAIModels } from '@/types/openai';
+import { NextApiRequest, NextApiResponse } from 'next';
 
-export const config = {
-  runtime: 'edge',
-};
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const session = await getServerSession(req, res, authOptions);
 
-const handler = async (req: Request): Promise<Response> => {
-  try {
-    const { key } = (await req.json()) as {
-      key: string;
-    };
-
-    const hardcodedModelId = 'gpt-3.5-turbo'; // Hardcoded model id
-    const hardcodedModel = {
-      id: hardcodedModelId,
-      name: OpenAIModels[hardcodedModelId].name
-    };
-
-    return new Response(JSON.stringify([hardcodedModel]), { status: 200 }); // Directly return hardcoded model
-  } catch (error) {
-    console.error(error);
-    return new Response('Error', { status: 500 });
+  if (!session) {
+    return res.status(401).json({ error: 'User is not authenticated' });
+  } else if (!session.user) {
+    return res.status(401).json({ error: 'User is not authenticated' });
+  } else if (!session.user.email) {
+    return res
+      .status(401)
+      .json({ error: 'User does not have an email address' });
   }
+  const userId = session.user.email;
+
+  const dataSource = await getDataSource();
+  const user = await getUser(dataSource, userId);
+
+  const allowed_models = user.allowed_models;
+  
+  if (!allowed_models) {
+    return res.status(418).json({ error: 'User does not have any allowed models' });
+  }
+  
+  const allowedModels: OpenAIModel[] = allowed_models.map((modelID: string) => {
+    return OpenAIModels[modelID as OpenAIModelID];
+  });
+
+  return res.status(200).json({ allowedModels });
+
+  
 };
 
 export default handler;
