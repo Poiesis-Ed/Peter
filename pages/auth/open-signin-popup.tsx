eb0e60570f6a0ff5cb54437ea3c16584f9a3780f
CodeCraft3r <72947633+codecraft3r@users.noreply.github.com>
2023-06-21 14:25:41 -0700

    add isIframed check, disable new auth if false

--- pages/auth/open-signin-popup.tsx
+++ pages/auth/open-signin-popup.tsx
@@ -2,9 +2,11 @@ import type { GetServerSidePropsContext, InferGetServerSidePropsType } from "nex
 import { getProviders, signIn, useSession } from "next-auth/react"
 import { getServerSession } from "next-auth/next"
 import { authOptions } from "../api/auth/[...nextauth]";
+import { useEffect, useState } from "react";
 
 export default function SignIn({ providers }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const { data: session, status } = useSession();
+  const [isIframed, setIsIframed] = useState<Boolean>(false)
   const openPopup = (providerId: string) => {
 
     const url = `/auth/signin?provider=${providerId}`; // Replace with your desired URL
@@ -13,12 +15,17 @@ export default function SignIn({ providers }: InferGetServerSidePropsType<typeof
 
     // Close the popup and remove the event listener when the window is closed
   };
+  useEffect(() => {
+    if (!(window===window.parent)) {
+      setIsIframed(true)
+    }
+  }, [])
 
   if (status === "authenticated") {
     window.location.href = "/"
     return null
 
-  } else if (status === "unauthenticated") {
+  } else if (isIframed && status === "unauthenticated") {
     return (
       <>
         {Object.values(providers).map((provider) => (
@@ -33,7 +40,21 @@ export default function SignIn({ providers }: InferGetServerSidePropsType<typeof
         ))}
       </>
     )
+  } else if (status === "unauthenticated") {
+    return (<>
+    {Object.values(providers).map((provider) => (
+      <div className="flex justify-center items-center h-screen" key={provider.name}>
+        <button
+          className="bg-blue-500 align-middle hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() => signIn(provider.id)}
+        >
+          Sign in with {provider.name}
+        </button>
+      </div>
+    ))}
+  </>)
   }
+
 }
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
